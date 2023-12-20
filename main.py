faff5292c5e313e2ee13fb8901322fe6268c1c76
rajarshi <rajobasu@gmail.com>
2023-12-20 12:41:17 +0530

    clean up main

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
-import time
-import torch
-
-from dataparser import get_data, get_random_word
-from models.similarity import SentenceSimilarityModel, MiniLM
-from models.toxicity import ToxicityModel, DetoxifyModel
-from morphers.fancy_morpher import QualityControlPipeline, RandomMorpher
+from dataparser import get_data
+from models.similarity import MiniLM
+from models.toxicity import DetoxifyModel
+from morphers.fancy_morpher import RandomMorpher
 from search.random_search import RandomSearch
 from storage.simple_storage import InMemStorage
-
-
-# def test_toxicity_model():
-#     start0 = time.time()
-#     toxicity_model = ToxicityModel()
-#     print(toxicity_model.predict("Why is sajal such a motherfucker"))
-#     start = time.time()
-#     print(start - start0)
-#
-
-def test_similarity_model():
-    start = time.time()
-    similarity_model = SentenceSimilarityModel()
-    print(similarity_model.predict("Sajal is a motherfucker", "sajal fucks good mothers"))
-    end = time.time()
-    print(end - start)
-
-
-def test_data():
-    print(get_data())
 
 
 def main():
     data_list = get_data()
 
-
-    # print(torch.cuda.get_device_name(torch.cuda.current_device()))
-    # exit(0)
+    # initialise models
     morpher = RandomMorpher()
-    # morpher.modify("hi")
-
-    print(get_random_word())
     toxic = DetoxifyModel()
     sent_sim = MiniLM()
 
